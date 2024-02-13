60406cc85491ea7e42b6edde491a0593288b9968
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2024-02-13 16:54:38 +0330

    completed server code

--- server.js
+++ server.js
@@ -2,45 +2,37 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
-const mongoose = require("mongoose");
-const File = require("./models/File");
+const connectDB = require("./database/connection");
 
-const port = process.env.PORT || 3000;
 const app = express();
 const upload = multer({});
+
+// set middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-mongoose.connect(process.env.MONGODB_URI);
+app.use(upload.single("file"));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.post("/upload", upload.single("file"), async (req, res) => {
-  const fileData = {
-    data: req.file.buffer,
-    originalName: req.file.originalname,
-  };
-
-  const file = await File.create(fileData);
-  res.json({
-    id: file.id,
-    name: file.originalName,
+// base API route
+const accessError = (req, res) => {
+  res.status(401).json({
+    message: "Access denied",
   });
-});
+  return;
+};
+app.route("/").all(accessError);
+app.use("/file", require("./routes/file.routes"));
 
-app.get("/file/:id", async (req, res) => {
-  const file = await File.findById(req.params.id);
-
-  const b64 = Buffer.from(file.data, "base64");
-
-  res.setHeader("Content-Type", "image/jpg");
-  res.setHeader("Content-Length", b64.length);
-  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-  res.send(b64);
-});
-
-app.listen(port, () => {
-  console.log("server run on port:", port);
-});
+(async function () {
+  try {
+    const PORT = process.env.PORT || 3000;
+    await connectDB(process.env.MONGODB_URI);
+    app.listen(PORT, () => {
+      console.log("The server running on port:", PORT);
+    });
+  } catch (error) {
+    console.log(error);
+    // make the process fail
+    process.exit(1);
+  }
+})();
